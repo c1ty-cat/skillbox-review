64a36f0ca03181725a655b30cd4c1a14d4982904
Ivan <sergeev.workmail@yandex.ru>
2024-05-27 09:28:58 +0300

    began topic 16

--- #1.cpp
+++ #1.cpp
@@ -1,62 +1,33 @@
 #include <iostream>
-#include <limits>
-# include <vector>
 
 using namespace std;
 
-    // Подсмотрел решение с эффективным алгоритмом,
-    // на его основе сделал вывод индексов
-
-    struct result {
-        int sum; int firstIdx; int lastIdx;
-    };
-
-
-result maximumSumSubarray(vector<int> arr, int n)
-{
-    result out;
-    // Initialize minimum prefix sum to 0.
-    int min_prefix_sum = 0;
-
-    // Initialize maximum subarray sum so
-    // far to -infinity.
-    int res = numeric_limits<int>::min();
-
-    // Initialize and compute the prefix
-    // sum array.
-    vector<int> prefix_sum(n);
-    for (int i = 1; i < n; i++)
-        prefix_sum[i] = prefix_sum[i - 1] + arr[i];
-
-    // loop through the array, keep track
-    // of minimum prefix sum so far and
-    // maximum subarray sum.
-    int startIdx = 0;
-    int endIdx = 0;
-    int oldRes = 0;
-    int oldMin = 0;
-    for (int i = 0; i < n; i++) {
-        oldRes = res;
-        oldMin = min_prefix_sum;
-        res = max(res, prefix_sum[i] - min_prefix_sum);
-        if(oldRes != res) endIdx = i;
-        if(res > (prefix_sum[i] - min_prefix_sum))
-        min_prefix_sum = min(min_prefix_sum, prefix_sum[i]);
-        if(oldMin != min_prefix_sum) startIdx = i+1;
-    }
-
-    out.lastIdx = endIdx;
-    out.firstIdx = startIdx;
-    out.sum = res;
-
-    return out;
+void printCurrentSpeed(float speed_value) {
+    char speed_str[10];
+    sprintf(speed_str, "%.1f", speed_value);
+    cout << "Speed: " << speed_str << "\n";
 }
 
+bool isRelativelyEqual(float lhs, float rhs, float epsilon = 0.01) {
+    return abs(lhs - rhs) < epsilon;
+}
+
+bool isProgramEnd(float speed_value) {
+    bool isDone = false;
+    if (isRelativelyEqual(speed_value, 0.0)) isDone = true;
+    else if (isRelativelyEqual(150.0, speed_value)) isDone = true;
+    else if(speed_value < 0.0 || speed_value > 150.0) isDone = true;
+    return isDone;
+}
 
 int main() {
-    vector<int> a = {-2, 1, -3, 4, -1 ,2 ,1 ,-5 ,4};
-    cout << "first IDX: " << maximumSumSubarray(a, a.size()).firstIdx << "\n";
-    cout << "last IDX: " << maximumSumSubarray(a, a.size()).lastIdx << "\n";
-    cout << "sum " << maximumSumSubarray(a, a.size()).sum << "\n";
+    float current_speed = 0;
+    float speed_delta = 0;
+    do {
+        cout << "Speed delta: ";
+        cin >> speed_delta;
+        current_speed += speed_delta;
+        printCurrentSpeed(current_speed);
+    } while (!isProgramEnd(current_speed));
     return 0;
 }
